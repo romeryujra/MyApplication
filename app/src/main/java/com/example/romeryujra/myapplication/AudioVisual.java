c587774847ef79b29484e55dd1f02df7cf6fe2a5
Romer Yujra Tucupa <romer.yujra@CI-LAPAZ.GOV.BO>
2017-10-13 10:18:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/romeryujra/myapplication/AudioVisual.java
@@ -0,0 +1,15 @@
+package com.example.romeryujra.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AudioVisual extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_visual);
+    }
+
+
+}
